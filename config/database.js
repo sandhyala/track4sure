06b6131abd65dabbc48f0688dc7c68403a1efd33
sandhyala <35270530+sandhyala@users.noreply.github.com>
2018-01-17 23:31:33 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://localhost:27017' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://lsgpsdb:Yq193njoAJSwVPWMU4IHHInH0eyEuF66oWosb9FSduDajoRza9LAefImTHwb8FulUZcUw31pnX5gpifFMEHqDA==@lsgpsdb.documents.azure.com:10255/?ssl=true' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
